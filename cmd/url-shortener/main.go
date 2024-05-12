3454a96a6d42739fa9edb857173c54e05a935f75
Sergei Ampilov <sergei.ampilov@gmail.com>
2024-05-12 23:21:01 +0300

    feat: storage ready

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"log/slog"
+	"os"
 	"url-shortener/internal/config"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/internal/storage/sqlite"
 	"url-shortener/logger"
 )
 
 func main() {
 	// config - cleanenv
 	cfg := *config.New()
-	// fmt.Printf("%+v\n", *cfg)
 
 	// logger - slog - import "log/slog"
 	log := logger.SetupLogger(cfg.Config.ENV)
@@ -18,6 +20,15 @@ func main() {
 	log.Debug("debug messages are enabled")
 
 	// storage - sqlite
+
+	stor, err := sqlite.New(cfg.Config.STORAGE_PATH)
+
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = stor
 
 	// router - chi, chi render
 
